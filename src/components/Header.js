c2ce6d10543d75b2d42158b2138009a00dcc212b
Dhruv Pant <dhruv.pant@tesco.com>
2026-01-30 09:17:17 +0530

    Added first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DisplaySVG from "../icons/Display.svg";
 import DownSVG from "../icons/down.svg";
-import "../classes/board.css"
+import "../classes/board.css";
 
 const Header = ({ onGroupChange, onSortChange }) => {
   const [isVisible, setIsVisible] = useState(false);
@@ -18,9 +18,12 @@ const Header = ({ onGroupChange, onSortChange }) => {
         </div>
         {isVisible && (
           <div class="dropdown-container">
-            <div className="dropdown">
+            <div class="dropdown">
               <label>Group by: </label>
-              <select className="dropdown-select" onChange={(e) => onGroupChange(e.target.value)}>    
+              <select
+                className="dropdown-select"
+                onChange={(e) => onGroupChange(e.target.value)}
+              >
                 <option value="user">User</option>
                 <option value="status">Status</option>
                 <option value="priority">Priority</option>
@@ -29,7 +32,10 @@ const Header = ({ onGroupChange, onSortChange }) => {
 
             <div className="dropdown">
               <label>Sort by: </label>
-              <select className="dropdown-select" onChange={(e) => onSortChange(e.target.value)}>
+              <select
+                className="dropdown-select"
+                onChange={(e) => onSortChange(e.target.value)}
+              >
                 <option value="priority">Priority</option>
                 <option value="title">Title</option>
               </select>
